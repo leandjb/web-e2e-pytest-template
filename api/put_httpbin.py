3a3496ac0f0dea65420d087f8767ffda56b4f555
leandjb <leandjb@gmail.com>
2025-03-22 12:05:47 -0500

    GET method for randomuser added
    GET randomuser list modified
    
    GET randomuser request improved
    
    other methods added

---
+++ api/put_httpbin.py
@@ -0,0 +1,15 @@
+import requests
+
+URL = 'https://httpbin.org/put'
+
+response = requests.put(URL,
+                        params={'name': 'Leandro'},
+                        headers={'version': '3.0'},
+                        data={'id': 1})
+
+if response.status_code == 200:
+    print(response.status_code)
+    print(response.text)
+
+else:
+    print(response.status_code)
